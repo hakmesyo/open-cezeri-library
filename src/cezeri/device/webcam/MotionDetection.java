85622702dd7d5f8c16633b856b1927e7583f1d3b
cezerilab <cezerilab@host.docker.internal>
2020-10-29 20:09:14 +0300

    motion detection to image and video rec were added

--- src/cezeri/device/webcam/MotionDetection.java
+++ src/cezeri/device/webcam/MotionDetection.java
@@ -14,6 +14,7 @@ import cezeri.factory.FactoryWebCam;
 public class MotionDetection {
     public static void main(String[] args) {
         //FactoryWebCam.openWebCam(0).startWithGUI().startMotionDetectionImage("C:\\recorded_images");
-        FactoryWebCam.openWebCam(0).startWithGUI().startMotionDetectionVideo("C:\\recorded_videos");        
+        FactoryWebCam.openWebCam(0).startWithGUI().startMotionDetectionVideo("C:\\recorded_videos");
+        
     }
 }
