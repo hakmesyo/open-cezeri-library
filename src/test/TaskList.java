bb0f3c28c4d82da367193b72dfa172d35b704a0e
cezerilab <cezerilab@DESKTOP-DALETJM.mshome.net>
2021-12-25 16:17:28 +0300

    new MLP approach was added

--- src/test/TaskList.java
+++ src/test/TaskList.java
@@ -15,6 +15,7 @@ public class TaskList {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+        
         String line;
         Process p = Runtime.getRuntime().exec("tasklist.exe");
         BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
